43a84d5c75639232e4b50335e9591292c31f021d
Marco 9726 <marcosarra9@gmail.com>
2023-06-16 15:30:42 +0200

    first commit

---
+++ src/components/Header.vue
@@ -0,0 +1,30 @@
+<script>
+name: 'Header';
+export default {};
+</script>
+
+<template>
+   <header>
+      <h1>Emoji Chef</h1>
+      <p>Selezione 3 emoji e crea la tua ricetta</p>
+      <div id="logo">👨🏻‍🍳</div>
+   </header>
+</template>
+
+<style lang="scss">
+header {
+   background-color: #2619aa;
+   color: #eeb9f5;
+   padding-top: 2rem;
+   h1 {
+      font-weight: 700;
+   }
+
+   #logo {
+      font-size: 6rem;
+      line-height: 1;
+      position: relative;
+      bottom: -3px;
+   }
+}
+</style>
